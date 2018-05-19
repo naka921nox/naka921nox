51706e5b8d97ad63001bda5151958877aeb8961a
keita <keita@LAPTOP-J9SUMTNM>
2018-05-19 22:31:00 +0900

    項目004：オートボクシングの例を追加

--- EffectiveJava/src/java/item005/Item005.java
+++ EffectiveJava/src/java/item005/Item005.java
@@ -20,5 +20,17 @@ public class Item005 {
 	Boolean isTest2 = Boolean.valueOf("true");
 
 	
+	// オートボクシングの悪い例
+	public void autoBoxing() {
+		Long sum = 0L;
+		
+		for (long i = 0; i <= Integer.MAX_VALUE; i ++) {
+			// ここで毎回int型に自動ボクシング
+			// 毎回不要なLong型がインスタンス化される
+			sum += 1;
+		}
+		System.out.println(sum);
+		// sumをlong型に変えるとパフォーマンス 43.2 sec が 6.8 sec に改善
+	}
 	
 }
