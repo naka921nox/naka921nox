aea340e17fed7d2e5c735bc8bd16341378a44153
keita nakano <keitatanakano@gmail.com>
2020-05-07 22:40:40 +0900

    Mementoパターン追加

---
+++ DesignPattern2/src/jp/memento/game/Gamer.java
@@ -0,0 +1,61 @@
+package jp.memento.game;
+
+import java.io.ObjectInputStream.GetField;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Gamer {
+    private int money;
+    private List<String> fruits = new ArrayList<>();
+    private Random random = new Random();
+    private static String[] fruitsname = {"リンゴ","ぶどう","バナナ","みかん",};
+
+    public Gamer (int money) {
+        this.money = money;
+    }
+    public int getMoney() {
+        return money;
+    }
+    public void bet() {
+        int dice = random.nextInt(6) + 1; // サイコロをふる
+        if (dice == 1) {
+            money += 100;
+            System.out.println("所持金が増えました。");
+        } else if (dice == 2) {
+            money /= 2;
+            System.out.println("所持金が半分になりました。");
+        } else if (dice == 6) {
+            String f = getFruit();
+            System.out.println("フルーツ " + f + " をもらいました");
+            fruits.add(f);
+        } else {
+            System.out.println("何も起こりませんでした。");
+        }
+    }
+    public Memento createMemento() {
+        Memento m = new Memento(money);
+        for (String f : fruits) {
+            if (f.startsWith("おいしい")) {
+                m.addFruit(f);
+            }
+        }
+        return m;
+    }
+    public void restoreMemento(Memento memento) {
+        this.money = memento.money;
+        this.fruits = memento.getFruit();
+    }
+    
+    @Override
+    public String toString() {
+        return "Gamer [fruits=" + fruits + ", money=" + money + ", random=" + random + "]";
+    }
+    private String getFruit() {
+        String prefix = "";
+        if (random.nextBoolean()) {
+            prefix = "おいしい";
+        }
+        return prefix + fruitsname[random.nextInt(fruitsname.length)];
+    }
+}
---
+++ DesignPattern2/src/jp/memento/game/Memento.java
@@ -0,0 +1,23 @@
+package jp.memento.game;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Memento {
+    int money;
+    ArrayList<String> fruits;
+
+    public int getMoney() {
+        return money;
+    }
+    Memento (int money) {
+        this.money = money;
+        this.fruits = new ArrayList<>();
+    }
+    void addFruit(String fruit) {
+        fruits.add(fruit);
+    }
+    List<String> getFruit() {
+        return (List<String>)fruits.clone();
+    }
+}
